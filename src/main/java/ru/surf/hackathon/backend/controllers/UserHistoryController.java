65770f96df82dcfb8949ccc58d8da6e7a993838e
MaksLachugin <lachugin@cs.vsu.ru>
2023-03-04 17:32:19 +0300

    userHistory

--- src/main/java/ru/surf/hackathon/backend/controllers/UserHistoryController.java
+++ src/main/java/ru/surf/hackathon/backend/controllers/UserHistoryController.java
@@ -2,13 +2,23 @@ package ru.surf.hackathon.backend.controllers;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.surf.hackathon.backend.entity.UserHistory;
+
+import ru.surf.hackathon.backend.services.UserHistoryService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1")
 @AllArgsConstructor
 @Slf4j
 public class UserHistoryController {
-
+    @Autowired
+    private final UserHistoryService userHistoryService;
+    @PostMapping("/user_history/")
+    List<UserHistory> getHistory(@RequestBody String hash){
+        return userHistoryService.findAllByHash(hash);
+    }
 }
